e2986e17e75de9f85ae61f25213b58973a6c5789
young1park <lossefanya@gmail.com>
2015-08-17 12:40:07 +0200

    Initial Commit

---
+++ OovooTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OovooTest
+//
+//  Created by Young One Park on 2015. 8. 17..
+//  Copyright (c) 2015년 young1park. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OovooTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OovooTest
+//
+//  Created by Young One Park on 2015. 8. 17..
+//  Copyright (c) 2015년 young1park. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+@end
